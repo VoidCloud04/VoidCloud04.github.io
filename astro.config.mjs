c4cb4d8041f879870fdd956f59d2f893126a22a3
VoidCloud04 <MrBacon470@outlook.com>
2026-03-05 14:04:49 -0600

    clean up of personal website. probably needs a redo some day

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mrbacon470.github.io',
+    site: 'https://voidcloud04.github.io',
 });
